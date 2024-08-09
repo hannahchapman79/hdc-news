44f1c1dfb25daf3e3e364f039f76f911b5127a8b
hannahchapman79 <hannahchapman1999@aol.com>
2024-08-09 11:40:56 +0100

    removes the unnecessary ArticleProvider component

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import './App.css'
 import Header from './components/Header'
 import Homepage from './components/Homepage'
 import { Route, Routes } from 'react-router-dom'
-import ArticleProvider from './components/ArticleProvider'
 import SingleArticle from './components/SingleArticle.jsx'
 import CommentsList from './components/CommentsList'
 import { TopicList } from './components/TopicList'
 import ErrorComponent from './components/Error'
+import ArticlesList from './components/ArticlesList'
 
 function App() {
 
@@ -16,7 +16,7 @@ function App() {
     <Header/>
     <Routes>
       <Route path="/" element={<Homepage/>} />
-      <Route path="/articles" element={<ArticleProvider/>} />
+      <Route path="/articles" element={<ArticlesList/>} />
       <Route path="/articles/:article_id" element={<SingleArticle/>} />
       <Route path="/articles/:article_id/comments" element={<CommentsList/>} />
       <Route path="/topics" element={<TopicList/>} />
--- src/components/ArticleProvider.jsx
+++ src/components/ArticleProvider.jsx
@@ -1,20 +1,20 @@
-import { useState } from "react"
-import ArticlesList from "./ArticlesList"
-import { SortBy } from "./SortBy"
+// import { useState } from "react"
+// import ArticlesList from "./ArticlesList"
+// import { SortBy } from "./SortBy"
 
 
-const ArticleProvider = () => {
-    const [currentArticles, setCurrentArticles] = useState([])
-    const [sortBy, setSortBy] = useState("")
-    const [order, setOrder] = useState("");
+// const ArticleProvider = () => {
+//     const [currentArticles, setCurrentArticles] = useState([])
+//     const [sortBy, setSortBy] = useState("")
+//     const [order, setOrder] = useState("");
 
-    const [isLoading, setIsLoading] = useState(false);
+//     const [isLoading, setIsLoading] = useState(false);
 
-    return (
-        <>
-            <ArticlesList currentArticles={currentArticles} setCurrentArticles={setCurrentArticles} isLoading={isLoading} setIsLoading={setIsLoading} sortBy={sortBy} setSortBy={setSortBy} order={order} setOrder={setOrder}/>
-        </>
-    )
-}
+//     return (
+//         <>
+//             <ArticlesList currentArticles={currentArticles} setCurrentArticles={setCurrentArticles} isLoading={isLoading} setIsLoading={setIsLoading} sortBy={sortBy} setSortBy={setSortBy} order={order} setOrder={setOrder}/>
+//         </>
+//     )
+// }
 
-export default ArticleProvider;
+// export default ArticleProvider;
